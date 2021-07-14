36c56e347044bae19378bb37e2efbb219102f3ec
Patrick <patrick@pop-os.localdomain>
2021-07-14 15:14:26 -0300

    Feita configuracao no eslint, criacao de uma classe de error

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,12 +1,31 @@
-import express from 'express';
+import cors from 'cors';
+import express, { NextFunction, Request, Response } from 'express';
+
+import AppError from '@shared/errors/AppError';
+
+import routes from './routes';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
-app.get('/', (request, response) => {
-  response.json({ message: 'Heeloo World' });
-});
+app.use(routes);
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server errro',
+    });
+  },
+);
 
 // eslint-disable-next-line no-console
 app.listen(3000, () => console.log('Server running on port 3000! 🔥'));
